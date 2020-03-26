a1b09a476e13f32e61bcbf5537c72d9f4d2a9cca
Andrew Cofano <andrewcofano@gmail.com>
2020-03-25 18:14:30 -0700

    music shit

---
+++ src/music/EpisodeSlider.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import SwipeableViews from 'react-swipeable-views';
+
+export default class EpisodeSlider extends Component {
+    render() {
+        return (
+            <SwipeableViews enableMouseEvents={true} axis="x">
+                <div style={Object.assign({})}>
+                    <div className="artworkWrapper">
+                        <div className="coverContainer flex-item">
+                            <img className="coverImage" src={require("../images/ep2-front.jpg")} alt="fuck"/>
+                        </div>
+                        <div className="tracklistContainer flex-item">
+                            <img className="tracklistImage" src={require("../images/ep2-back.jpg")} alt="fuck"/>
+                        </div>
+                    </div>
+                </div>
+                <div style={Object.assign({})}>
+                    <div className="artworkWrapper">
+                        <div className="coverContainer flex-item">
+                            <img className="coverImage" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                        </div>
+                        <div className="tracklistContainer flex-item">
+                            <img className="tracklistImage" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                </div>
+
+            </SwipeableViews>
+    );
+}}
+
