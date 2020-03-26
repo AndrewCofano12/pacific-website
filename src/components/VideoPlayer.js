f658d840a4a3481ef7ae0601b8fd9eee0e7255dd
aplau95 <aplau.95@gmail.com>
2020-03-25 21:53:58 -0700

    working video scaling needs centering formula

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,100 +1,74 @@
-import React, { Component, Text } from 'react';
+import React, { Component, Text, useCallback } from 'react';
 import "./VideoPlayer.css"
 import request from "../node/vimeoApi"
 import Vimeo from '@vimeo/player'
+
 
 export default class VideoPlayer extends Component {
     constructor(props) {
         super(props)
         
         let player 
-        this.state = {seconds: 0}
-
-    }
-    
-    componentDidMount() {    
-        // const iframe = document.querySelector('iframe');
-        // this.player = new Vimeo(iframe)
-        // this.player.on('progress', () => this.player.getCurrentTime().then((seconds) => {
-        //     // console.log(seconds)
-        //     this.setState({seconds: seconds})
-        // })); 
-        var options01 = {
-            id: "https://player.vimeo.com/video/395326240?background=1&muted=0",
-            width: window.width,
-            height: window.height
-          };
-        //   var options02 = {
-        //     url: {video02_url},
-        //     width: {video02_width}
-        //   };
-      
-          var video01Player = new Vimeo('video01_name', options01);
-        //   var video02Player = new Vimeo.Player('{video02_name}', options02);
-      
-          video01Player.setVolume(0);
-        //   video02Player.setVolume(0);
-      
-          video01Player.on('play', function() {
-            console.log('Played the first video');
-          });
-
-        //   video01Player.
+        this.state = {seconds: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        }
     }
 
-    // updateTime() {
-    //     console.log("firing")
-    //     this.player.getCurrentTime().then((seconds) => {
-    //         console.log(seconds)
-    //         // this.setState({seconds: seconds})
-    //     }) 
-    //     console.log("asdfasdf")
-    // }
+    handleResize() {
+      // window shrinks horizontally
+      const height = window.innerHeight
+      const width = window.innerWidth
+      
+      // 16:9 aspect ratio
+      if(height / width > .5625) {
+        this.setState({width: height / .5625, height: height }) //shrink horizontally
+      } else {
+        this.setState({width: width, height: width *.5625 }) //shrink vertically
+      }
+    }
 
-    // fuck = () => {
-    //     if(this.player === null) {
-    //         console.log("not here yet")
-    //     } else {
-    //         this.player.getPaused().then((paused) => {
-    //             if(paused) {
-    //                 this.player.play()
-    //             } else {
-    //                 this.player.pause()
-    //             }
-    //         })
-    //     }
-    // }
-        
+    componentDidMount() {  
+      this.handleResize()
+      window.addEventListener("resize", this.handleResize.bind(this))
+      var iframe = document.querySelector('iframe');
+      this.player = new Vimeo(iframe)
+
+      this.handlePlayPause(this.player)
+      this.player.on('play', function() {
+        console.log('Played the video');
+      });
+    }
+
+
+    handlePlayPause(player) {
+      player.getPaused().then((paused) => {
+        if(paused) {
+          player.play()
+        } else {
+          player.pause()
+        }
+      });
+    }
+
     render() {
-
-       
-
         return(
-            <div>
-                <div id="video01_name"></div>
+            <div style={{height: "100%", width: "100", display: "block", backgroundColor: 'black'}}>
+              <iframe 
+              title="video" 
+              src="https://player.vimeo.com/video/395326240?autoplay=true&controls=false" 
+              width={this.state.width} 
+              height={this.state.height}
+              top="0"
+              left="0"
+              frameborder="0" 
+              webkitallowfullscreen 
+              mozallowfullscreen 
+              allowfullscreen
+              />
+              <button onClick={() => this.handlePlayPause(this.player)} style={{position: 'absolute', top: 40, left: 100}}>Click Me!</button>
+                {/* <div style={{height: this.state.height, width: this.state.width}} id="video01_name"></div> */}
             </div>
         );
-    //     return (
-    //         // <div className="videoPlayer">
-    //         <div class="video-background">
-    // <div class="video-foreground">
-    //             {/* <button onClick={() => this.fuck()}>Activitate</button> */}
-    //             {/* <text style={{position: "absolute"}}>{this.state.seconds}</text> */}
-    //             <iframe 
-    //             // position="absolute"
-    //                 title="video"
-    //                 src="https://player.vimeo.com/video/395326240?background=1&muted=0" 
-    //                 // width={window.width} 
-    //                 // height={window.height} 
-    //                 // left="0"
-    //                 // top="0"
-    //                 // right="auto"
-    //                 frameBorder="0" 
-    //                 allow="autoplay; fullscreen"/>
-    //         </div>
-    //         </div>
-            
-    //     );
     }
-        
 }
