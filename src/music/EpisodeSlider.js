ca42d35c64b4977346416f895e5dbcbcabb790ac
aplau95 <aplau.95@gmail.com>
2020-03-25 21:53:58 -0700

    basic music page

--- src/music/EpisodeSlider.js
+++ src/music/EpisodeSlider.js
@@ -1,33 +1,91 @@
 import React, { Component } from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import './EpisodeSlider.css'
+import Pagination from '../components/pagination/Pagination';
 
 export default class EpisodeSlider extends Component {
-    render() {
-        return (
-            <SwipeableViews className="swipeableView" enableMouseEvents={true} axis="x">
-                {/* <div style={Object.assign({})}> */}
-                    <div className="artworkWrapper">
-                        {/* <div className="coverContainer flex-item"> */}
-                            <img className="coverImage" src={require("../images/ep2-front.jpg")} alt="fuck"/>
-                        {/* </div> */}
-                        {/* <div className="tracklistContainer flex-item"> */}
-                            <img className="tracklistImage" src={require("../images/ep2-back.jpg")} alt="fuck"/>
-                        {/* </div> */}
-                    </div>
-                {/* </div> */}
-                {/* <div style={Object.assign({})}> */}
-                    <div className="artworkWrapper">
-                        <div className="coverContainer flex-item">
-                            <img className="coverImage" src={require("../images/ep1-cover.png")} alt="fuck"/>
-                        </div>
-                        <div className="tracklistContainer flex-item">
-                            <img className="tracklistImage" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
-                        </div>
-                    </div>
-                {/* </div> */}
+    state = {
+        index: 0,
+        centerWidth: window.innerWidth
+      };
 
-            </SwipeableViews>
+    handleChangeIndex = index => {
+        this.setState({
+          index,
+        });
+      };
+
+    render() {
+        const { index } = this.state;
+        
+        return (
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <SwipeableViews className="swipeableView" enableMouseEvents={true} axis="x" 
+                index={index} 
+                onChangeIndex={this.handleChangeIndex}>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep2-front.jpg")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep2-back.jpg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                    <div style={Object.assign({})}>
+                        <div className="artworkWrapper">
+                            <img className="coverImage noselect" src={require("../images/ep1-cover.png")} alt="fuck"/>
+                            <img className="tracklistImage noselect" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
+                        </div>
+                    </div>
+                </SwipeableViews>
+                <Pagination dots={10} index={index} onChangeIndex={this.handleChangeIndex} />                
+            </div>
     );
 }}
 
