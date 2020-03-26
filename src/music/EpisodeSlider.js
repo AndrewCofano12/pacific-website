fdc32a2474db1989eb024e35bd03f5daed2a905e
Andrew Cofano <andrewcofano@gmail.com>
2020-03-25 18:48:28 -0700

    more yarn shit

--- src/music/EpisodeSlider.js
+++ src/music/EpisodeSlider.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import SwipeableViews from 'react-swipeable-views';
+import './EpisodeSlider.css'
 
 export default class EpisodeSlider extends Component {
     render() {
         return (
-            <SwipeableViews enableMouseEvents={true} axis="x">
-                <div style={Object.assign({})}>
+            <SwipeableViews className="swipeableView" enableMouseEvents={true} axis="x">
+                {/* <div style={Object.assign({})}> */}
                     <div className="artworkWrapper">
-                        <div className="coverContainer flex-item">
+                        {/* <div className="coverContainer flex-item"> */}
                             <img className="coverImage" src={require("../images/ep2-front.jpg")} alt="fuck"/>
-                        </div>
-                        <div className="tracklistContainer flex-item">
+                        {/* </div> */}
+                        {/* <div className="tracklistContainer flex-item"> */}
                             <img className="tracklistImage" src={require("../images/ep2-back.jpg")} alt="fuck"/>
-                        </div>
+                        {/* </div> */}
                     </div>
-                </div>
-                <div style={Object.assign({})}>
+                {/* </div> */}
+                {/* <div style={Object.assign({})}> */}
                     <div className="artworkWrapper">
                         <div className="coverContainer flex-item">
                             <img className="coverImage" src={require("../images/ep1-cover.png")} alt="fuck"/>
@@ -24,7 +25,7 @@ export default class EpisodeSlider extends Component {
                             <img className="tracklistImage" src={require("../images/ep1-back.jpeg")} alt="fuck"/>
                         </div>
                     </div>
-                </div>
+                {/* </div> */}
 
             </SwipeableViews>
     );
