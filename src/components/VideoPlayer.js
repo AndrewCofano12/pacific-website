78188202f3d75dfdc02205eaba7d564a5ab38500
aplau95 <aplau.95@gmail.com>
2020-04-08 17:48:33 -0700

    video functionality

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -9,10 +9,13 @@ export default class VideoPlayer extends Component {
         super(props)
         
         let player 
-        this.state = {seconds: 0,
+        this.state = {
+          seconds: 0,
           width: window.innerWidth,
           height: window.innerHeight,
-          volume: .5
+          volume: .5,
+          playbackPosition: 0,
+          left: 0
         }
     }
 
@@ -23,7 +26,10 @@ export default class VideoPlayer extends Component {
       
       // 16:9 aspect ratio
       if(height / width > .5625) {
+        console.log(this.state.left + (this.state.width - width) * -.5)
+        this.setState({left: this.state.left + (this.state.width - width) * -.5})
         this.setState({width: height / .5625, height: height }) //shrink horizontally
+        
       } else {
         this.setState({width: width, height: width *.5625 }) //shrink vertically
       }
@@ -35,6 +41,12 @@ export default class VideoPlayer extends Component {
       var iframe = document.querySelector('iframe');
       this.player = new Vimeo(iframe)
       this.player.setVolume(this.state.volume)
+
+      // var playerr = document.getE('video')
+      this.player.on('timeupdate', (event) => {
+        this.setState({playbackPosition: event.seconds})
+        // console.log(this.state.playbackPosition)
+      })
 
       this.player.on('play', function() {
         console.log('Played the video');
@@ -50,6 +62,10 @@ export default class VideoPlayer extends Component {
           player.pause()
         }
       });
+    }
+
+    onVideoProgress(player) {
+      player.getCurrentTime()
     }
 
     increaseVolume(player) {
@@ -69,22 +85,24 @@ export default class VideoPlayer extends Component {
             <div style={{height: "100%", width: "100", display: "block", backgroundColor: 'black'}}>
               <iframe 
               title="video" 
-              src="https://player.vimeo.com/video/395326240?autoplay=true&controls=false" 
+              src="https://player.vimeo.com/video/395326240?autoplay=true&controls=false&muted=true" 
               width={this.state.width} 
               height={this.state.height}
               top="0"
-              left="0"
-              frameborder="0" 
-              webkitallowfullscreen 
-              mozallowfullscreen 
-              allowfullscreen
-              fullscreen
+              left={this.state.left}
+              frameBorder="0" 
+              webkitallowfullscreen="true"
+              mozallowfullscreen="true"
+              allowFullScreen={true}
+              fullscreen="true"
               allow="autoplay"
               />
               <button onClick={() => this.handlePlayPause(this.player)} style={{position: 'absolute', top: 40, left: 100}}>Play/Pause</button>
               <button onClick={() => this.increaseVolume(this.player)} style={{zIndex: 20, position: 'absolute', top: 80, left: 100}}>Increase Volume</button>
               <button onClick={() => this.decreaseVolume(this.player)} style={{zIndex: 20, position: 'absolute', top: 120, left: 100}}>Decrease Volume</button>
-                {/* <div style={{height: this.state.height, width: this.state.width}} id="video01_name"></div> */}
+              {/* <input style={{position: 'absolute', bottom: 50, left: 100, right: 100}} type="range" min="1" max="100" value={this.state.playbackPosition} className="slider" id="myRange"></input> */}
+              {/* <input type="range" value="50" max="100"></input>
+              <progress value="50" max="100"></progress> */}
             </div>
         );
     }
