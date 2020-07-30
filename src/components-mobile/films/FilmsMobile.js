dc388b9d1981fa8ddf6b33010067d41057aa2725
Andrew Cofano <andrewcofano@gmail.com>
2020-07-29 20:51:41 -0700

    mobile films page. need to figure out vimeo player

---
+++ src/components-mobile/films/FilmsMobile.js
@@ -0,0 +1,82 @@
+import React, { Component,  useState  } from 'react';
+import NavigationHeader from '../../components/NavigationHeader';
+import './FilmsMobile.css';
+import Vimeo from "@vimeo/player";
+
+export default class Films extends Component {  
+    videoPlayers = [];
+    video2Player;
+    video3Player;
+    volume = 30;
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      filmsObject: this.props.dbdata
+    }
+  }
+
+  componentDidMount() {
+    this.state.filmsObject.filmsEntries.map((entry, i) => {
+        var idString = 'film'
+        var options = {
+            playsinline: false,
+        }
+        var videoPlayer = new Vimeo(`film${i}`, options);
+        videoPlayer.setVolume(this.volume);
+        this.videoPlayers.push(videoPlayer);
+    });
+
+
+  }
+
+
+  handlePlay = (i) => {
+    console.log(i)
+    console.log('film'+ i);
+    var player = document.getElementById(`film${i}`);
+    console.log(player)
+    player.style["display"] = 'block';
+    this.videoPlayers[i].play();
+  }
+
+  render() {
+
+    // console.log("this.props.entries is not null " + this.props.entries.id != null)
+
+    // this.props.entries.map ((entry) => console.log(entry.id))
+    // const [videoFilePath, setVideoFileURL] = useState(null);
+    // const handleVideoUpload = (event) => {
+    //   setVideoPath(URL.createObjectURL('../../public/test.MP4'));
+    //   };
+    
+    return (
+      <div className="videoPlayer">
+        <NavigationHeader formatString="lightFormat" page="films"/>
+        <video 
+        className="testVideo"
+        autoPlay={true}
+        loop={true} 
+        muted={true}
+        src="http://danielcaesar.com/admin/wp-content/uploads/2016/11/Clip-driver.mp4"
+        >
+
+        </video>       
+        <div className="films-linkContainer">
+
+        {this.state.filmsObject.filmsEntries.map((entry, i) => {
+            console.log(`film${i}`);
+            return (
+                <div className="filmSeletion" >
+                    <div onClick={() => this.handlePlay(i)}>{entry.name}</div>
+                    <div style={{display: 'none'}} id={`film${i}`} data-vimeo-url={entry.src}></div>
+                </div>
+            );
+            })};
+        </div>
+    </div>
+
+    );
+  }
+}
---
+++ src/components-mobile/films/VideoPlayerMobile.js
@@ -0,0 +1,229 @@
+import React, { Component } from "react";
+import "./VideoPlayer.css";
+import Vimeo from "@vimeo/player";
+import Fullscreen from "react-full-screen";
+import PlayerController from "./PlayerController";
+import CreditsOverlay from "./CreditsOverlay"
+
+export default class VideoPlayer extends Component {
+//   timer;
+//   seekbar;
+  volume = 30;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    //   width: window.innerWidth,
+    //   height: window.innerHeight,
+    //   volume: 30,
+    //   playbackPosition: 0,
+    //   left: 0,
+    //   top: 0, 
+    //   right: 0,
+    //   showPlayerControls: false,
+      playbackLength: 0,
+    //   isFull: false,
+      isPlaying: true,
+      looped: false,
+    };
+  }
+
+//   showPlayerControls() {
+//     if (!this.state.showPlayerControls) {
+//       if (this.state.isPlaying) {
+//       clearTimeout(this.timer);
+//       this.hideWithTimer();
+//       }
+
+//       this.setState({ showPlayerControls: true });
+//     }
+//   }
+
+//   hidePlayerControls() {
+//     this.setState({ showPlayerControls: false });
+//   }
+
+//   hideWithTimer() {
+//     this.timer = setTimeout(() => {
+//       this.hidePlayerControls();
+//     }, 3000);
+
+//   }
+
+//   handleResize() {
+//     this.setState({
+//       width: window.innerWidth,
+//       height: window.innerHeight
+//     });
+//   }
+
+//   pauseVideo() {
+//     this.player.pause().then(() => {
+//       this.setState({
+//         isPlaying: false,
+//       });
+//       clearTimeout(this.timer);
+//       this.showPlayerControls();
+//     });
+//   }
+
+  playVideo() {
+    this.player.play().then(() => {
+      this.setState({
+        isPlaying: true,
+        looped: false,
+      });
+      this.hideWithTimer();
+    });
+  }
+
+  componentDidMount() {
+    // this.handleResize();
+
+    var iframe = document.querySelector("iframe");
+    this.player = new Vimeo(iframe);
+
+    this.player.setVolume(this.volume);
+
+    this.attachListeners();
+    this.attachVideoListener();
+  }
+
+  attachVideoListener() {
+    this.player.getDuration().then((duration) => {
+      this.setState({ playbackLength: duration - .5});
+    });
+
+    this.player.on("play", function () {
+      this.setState({ isPlaying: true });
+
+    });
+
+    this.player.on("timeupdate", (event) => {
+      this.setState({ playbackPosition: event.seconds });
+      if (this.state.playbackLength < event.seconds) {
+        this.goBackToBeginning();
+      }
+    });
+  }
+
+//   attachListeners() {
+//     window.addEventListener("resize", this.handleResize.bind(this));
+//     window.addEventListener("mousemove", this.showPlayerControls.bind(this));
+//   }
+
+  goBackToBeginning() {
+    
+    this.player.pause();
+    this.player.setCurrentTime(0);
+    // this.player.updateVideoPosition(.1)
+    this.setState({
+      isPlaying: false,
+      looped: true,
+    });
+    // this.showPlayerControls.bind(this);
+  }
+
+  /** Funcs to pass as props */
+
+//   updateVideoPosition = (time) => {
+//     this.player.setCurrentTime(time);
+//   };
+
+//   goFull = () => {
+//     this.setState({ isFull: true });
+//   };
+
+//   exitFull = () => {
+//     this.setState({ isFull: false });
+//   };
+
+  handleExit = () => {
+    this.props.history.push("/films");
+  };
+
+  handlePlayPause = () => {
+    if (this.state.isPlaying) {
+      this.pauseVideo();
+    } else {
+      this.playVideo();
+    }
+  };
+
+  renderVideoFrame() {
+    return (
+        <video
+        className="frame"
+        title="video"
+        src={this.props.src}
+        backgroundColor="black"
+        mozallowfullscreen="true"
+        allowFullScreen={true}
+        playsinline
+        fullscreen="true"
+        allow="autoplay"
+      />
+      
+    );
+  }
+
+//   renderControls() {
+//     return (
+//       <div>
+//         <PlayerController
+//           handleExit={this.handleExit}
+//           isShowing={this.state.showPlayerControls}
+//           isPlaying={this.state.isPlaying}
+//           handlePlayPause={this.handlePlayPause}
+//           playbackPosition={this.state.playbackPosition}
+//           playbackLength={this.state.playbackLength}
+//           updateVideoPosition={this.updateVideoPosition}
+//           isFullScreen={this.state.isFull}
+//           goFull={this.goFull}
+//           exitFull={this.exitFull}
+//           looped={this.state.looped}
+//           filmObject={this.props.filmObject}
+//         />
+//       </div>
+//     );
+//   }
+
+  render() {
+    return (
+      <div>
+      {/* <Fullscreen
+          enabled={this.state.isFull}
+          onChange={(isFull) => this.setState({ 
+            isFull
+          })}
+        > */}
+          
+      {/* <div
+        className="films-videoPage"
+        style={{
+          height: "100%",
+          width: "100vw",
+          display: "block",
+          backgroundColor: "black",
+        }}
+      > */}
+        <div
+        
+        // className="films-overlay"
+        // style={{
+        //   height: "100%",
+        //   width: "100vw"
+        // }}
+        >
+        
+
+            {this.renderVideoFrame()}
+            {/* {this.renderControls()} */}
+            </div>
+
+      </div>
+    //   </Fullscreen>
+    //   </div>
+    );
+  }
+}
